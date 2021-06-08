99e4adf8b65a079a519d9988bd8cadbff53e66ed
lorenzokata <catalanolorenzo95@gmail.com>
2021-06-09 00:10:19 +0200

    bonus con If

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 document.getElementById('start').addEventListener('click', function(){
 
+    var difficolta = document.getElementById('difficolta').value;
+
+    var maxRange = 100;
+
+    if (difficolta == 2) {
+        maxRange = 80;
+    }
+
+    if (difficolta == 3) {
+        maxRange = 50;
+    }
+
+    console.log(maxRange);
+
     var arrayComputer = [];
 
     var lengthArrayComputer = 16;
 
     while (arrayComputer.length < lengthArrayComputer) {
 
-        var numPcRandom = Math.floor(Math.random()*100)+1;
+        var numPcRandom = Math.floor(Math.random()*maxRange)+1;
 
         if (!arrayComputer.includes(numPcRandom)) {
 
@@ -21,12 +35,19 @@ document.getElementById('start').addEventListener('click', function(){
 
     var arrayUtente = [];
 
-    while (!numeroPresente && arrayUtente.length < (100 - lengthArrayComputer)) {
+    while (!numeroPresente && arrayUtente.length < (maxRange - lengthArrayComputer)) {
 
         var numeroUtente = parseInt(prompt('Inserisci il numero'));
 
-        while (arrayUtente.includes(numeroUtente) || 1 > numeroUtente > 100) {
-            numeroUtente = parseInt(prompt('Inserisci un numero diverso da quelli inseriti in precedenza'));
+        while (arrayUtente.includes(numeroUtente) || numeroUtente > maxRange || numeroUtente < 1) {
+
+            if (arrayUtente.includes(numeroUtente)) {
+                numeroUtente = parseInt(prompt('Inserisci un numero diverso da quelli inseriti in precedenza'));
+            }
+            
+            if (numeroUtente > maxRange || numeroUtente < 1) {
+                numeroUtente = parseInt(prompt('Inserisci un numero compreso tra 1 e ' + maxRange));
+            }
         }
 
         if (!arrayComputer.includes(numeroUtente)) {
@@ -40,7 +61,7 @@ document.getElementById('start').addEventListener('click', function(){
 
     console.log(arrayUtente);
 
-    if (arrayUtente.length == (100 - lengthArrayComputer)) {
+    if (arrayUtente.length == (maxRange - lengthArrayComputer)) {
         console.log('HAI VINTO!!!!');
     } else{
         console.log('BOOOOM!!!! Il tuo punteggio é: ' + arrayUtente.length);
