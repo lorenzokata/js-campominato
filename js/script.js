754a4c99f0b6698da7910a3b8d99dad7b06bfb3c
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-08 16:54:29 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
-var arrayComputer = [];
+document.getElementById('start').addEventListener('click', function(){
 
-var lengtharrayComputer = 16
+    var arrayComputer = [];
 
-while (arrayComputer.length < lengtharrayComputer) {
+    var lengthArrayComputer = 16;
 
-    var numPcRandom = Math.floor(Math.random()*100)+1;
+    while (arrayComputer.length < lengthArrayComputer) {
 
-    if (!arrayComputer.includes(numPcRandom)) {
+        var numPcRandom = Math.floor(Math.random()*100)+1;
 
-        arrayComputer.push(numPcRandom);
+        if (!arrayComputer.includes(numPcRandom)) {
+
+            arrayComputer.push(numPcRandom);
+        }
     }
-}
 
-console.log(arrayComputer);
+    console.log(arrayComputer);
+
+
+    var numeroPresente = false;
+
+    var arrayUtente = [];
+
+    while (!numeroPresente && arrayUtente.length < (100 - lengthArrayComputer)) {
+
+        var numeroUtente = parseInt(prompt('Inserisci il numero'));
+
+        while (arrayUtente.includes(numeroUtente)) {
+            numeroUtente = parseInt(prompt('Inserisci un numero diverso da quelli inseriti in precedenza'));
+        }
+
+        if (!arrayComputer.includes(numeroUtente)) {
+            arrayUtente.push(numeroUtente);
+        } else{
+            numeroPresente = true;
+        }
+
+        console.log(numeroPresente);
+    }
+
+    console.log(arrayUtente);
+
+    if (arrayUtente.length == (100 - lengthArrayComputer)) {
+        console.log('HAI VINTO!!!!');
+    } else{
+        console.log('Il tuo punteggio é: ' + arrayUtente.length);
+    }
+
+});
 
